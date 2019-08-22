565acce6ad894efe995c3816cb26ca07b0104f88
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2019-08-22 13:22:01 -0400

    1977

--- Code/cluster_visualizer.py
+++ Code/cluster_visualizer.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-df_visual_data = pd.read_csv('clustered_2025.csv')
+df_visual_data = pd.read_csv('clustered_1977.csv')
 df_visual_data = df_visual_data.groupby('sign_id')
 
 sign_to_plot   = int(input("Please enter the sign you want to plot"))
@@ -32,7 +32,7 @@ for i in set(clusters):
 
 	temp_df=df_visual_data.groupby('cluster_group')
 	temp_df=temp_df.get_group(int(i))
-	ax.scatter(temp_df['lidar_lat'],temp_df['lidar_long'], temp_df['lidar_alt'],s=20, c=colors[i], marker=markers[i])
+	ax.scatter(temp_df['lidar_lat'],temp_df['lidar_long'], temp_df['lidar_alt'],s=20, c='orange', marker='*')
 
 
 # for i in set(clusters):
